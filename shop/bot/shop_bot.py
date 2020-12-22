fe7c362ecdd56daff5f8a4771cf8fc57eea00a32
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-12-22 21:25:33 +0200

    add decode utf-8

--- shop/bot/shop_bot.py
+++ shop/bot/shop_bot.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 @app.route(WEBHOOK_URI, methods=['POST'])
 def handle_webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data()
+        json_string = request.get_data().decode('utf-8')
         update = Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
