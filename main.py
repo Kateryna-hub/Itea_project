e91b78413077a2c6a2b3f486bfea96b2f4dc59bd
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-12-25 19:49:58 +0200

    fix rest

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from shop.bot.shop_bot import bot, app
 import time
 from shop.bot.config import WEBHOOK_URL
-from shop.api.restful import app_rest
+
 
 #bot.polling()
 bot.remove_webhook()
 time.sleep(0.5)
 bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem'))
-app.run()
+#app.run()
 
-app_rest.run(debug=True)
--- shop/api/restful.py
+++ shop/api/restful.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
 from .resources import Resource
+from shop.bot.shop_bot import app
 
-app_rest = Flask(__name__)
-api = Api(app_rest)
+#app_rest = Flask(__name__)
+api = Api(app)
 
 
 api.add_resource(Resource, '/tg', '/tg/categories/<string:category>', '/tg/products/<string:product>',
