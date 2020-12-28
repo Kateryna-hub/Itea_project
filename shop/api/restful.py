e991b1cea01f0d4bff2add81a5e2fa9014942a40
katerynabrovko@gmail.com <katerynabrovko@gmail.com>
2020-12-29 00:15:12 +0200

    fix

--- shop/api/restful.py
+++ shop/api/restful.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Api
-from .resources import Resource
+from .resources import Resource, ShopResource
 from shop.bot.shop_bot import app
 
 #app_rest = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(Resource, '/tg', '/tg/categories/<string:category>', '/tg/products/<string:product>',
+api.add_resource(ShopResource, '/tg', '/tg/categories/<string:category>', '/tg/products/<string:product>',
                  '/tg/product/<string:id>', '/tg/user/<string:telegram_id>')
